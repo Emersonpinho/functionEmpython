001546dd836e841eed740d4571657405eaaa09fa
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 20:54:29 -0300

    add function to calculate average of three grades

--- declarandoFunction.py
+++ declarandoFunction.py
@@ -1,5 +1,9 @@
-def somarNumeros(a, b):
-    soma = a + b
-    return soma
+def calcularMedia(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) / 3
+    print('Sua primeira nota foi:', nota1)
+    print('Sua segunda nota foi:', nota2)
+    print('Sua terceira nota foi:', nota3)
+    print(f'A media entre esssas é: {media:.2f}')
+   
 
-print(somarNumeros(2, 3))
+print(calcularMedia(7, 8, 7))
