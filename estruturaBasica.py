1bd277a9084b5703af57ed3863700a0ba6863865
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 21:22:15 -0300

    enhance function documentation with additional explanations on parameters and return values

--- estruturaBasica.py
+++ estruturaBasica.py
@@ -3,9 +3,17 @@ def nomeDaFuncao(parametro):
     return resultadoOpsicional
 
 #  - Oque é def?
-
+#
 #     def é a palavra chave que indica e inicia a função
 #
 #  - Oque é nomeDaFuncao?
-
+#
 #    nomeDaFuncao é o nome que você deseja dar a sua função. Deve seguir as regras de nomenclatura de           #    indentificadores em Python
+#
+#  - Oque é parametro?
+#
+#   parametro é argumentos que você desejar passar para a função. Elas são opcionais e pode ser zero ou mais
+#
+#  - Oque é o return?
+#
+#   return devolve o valor ou resultado da função. Ele é opcional e pode ser omitido
