3aeb463c8f3fb460ee6965b2d074b8370201d065
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 21:30:17 -0300

    add multiplication function and clarify parameter usage in examples

--- funcaoSimples.py
+++ funcaoSimples.py
@@ -2,4 +2,11 @@ def somar(a, b):
     """retorna a soma de dois numeros"""
     return a + b
 
-print(somar(10, 10))
+'''print(somar(10, 10))'''
+
+# se você não quiser parametros, basta deixar os parenteses vazios
+
+def multiplicar():
+    return 20 * 2
+
+print(multiplicar()) # erro, pois a função não tem parametros. você não pode inserir dados no print se não tiver parametros
