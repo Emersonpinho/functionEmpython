f0a5edb72e04a5cec8930e0193f42e1b4ff9b388
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 21:18:54 -0300

    muitas alterações

--- estruturaBasica.py
+++ estruturaBasica.py
@@ -1,3 +1,11 @@
 def nomeDaFuncao(parametro):
     # Corpo ou bloco de código
     return resultadoOpsicional
+
+#  - Oque é def?
+
+#     def é a palavra chave que indica e inicia a função
+#
+#  - Oque é nomeDaFuncao?
+
+#    nomeDaFuncao é o nome que você deseja dar a sua função. Deve seguir as regras de nomenclatura de           #    indentificadores em Python
