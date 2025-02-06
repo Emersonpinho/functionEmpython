d70a7b8303bced3a7e169519340c2499b2e44ed0
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 21:42:13 -0300

    add sauadacao2 function for printing messages with name parameter

--- praticaDefinirFuncao.py
+++ praticaDefinirFuncao.py
@@ -2,3 +2,8 @@ def saudacao(mensagem, nome="Emerson"):
     return f"{mensagem}, {nome}"
 
 print(saudacao("Olá"))# Olá, Emerson
+
+def sauadacao2(mensagem, nome):
+     print(f"{mensagem}, {nome}")
+
+sauadacao2('ola', 'Emerson pinho')
