53ca5ced816cd724eb2a8b4c7e98148df579a381
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 21:32:31 -0300

    add division function with default parameters and update print examples

--- funcaoSimples.py
+++ funcaoSimples.py
@@ -10,3 +10,10 @@ def multiplicar():
     return 20 * 2
 
 print(multiplicar()) # erro, pois a função não tem parametros. você não pode inserir dados no print se não tiver parametros
+
+# você pode definir valores padrões para os parametros
+def dividir(a=2, b=2):
+    return a / b
+
+print(dividir())
+print(dividir(10, 2)) 
